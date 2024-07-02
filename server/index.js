eedc58f8ae087af9126a7e49d0744949a5b571e1
OFFICIALNITIN <official.nitin105@gmail.com>
2024-07-02 15:57:12 +0530

    made some changes

--- server/index.js
+++ server/index.js
@@ -34,6 +34,8 @@ app.use(helmet());
 app.use((req, res, next) => {
   res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
   res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  res.setHeader("Cross-Origin-Resource-Policy", "same-site");
+
   next();
 });
 
