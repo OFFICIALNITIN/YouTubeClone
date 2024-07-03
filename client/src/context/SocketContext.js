ccf8c51a37c27ab91eae73b767146925252364a6
OFFICIALNITIN <official.nitin105@gmail.com>
2024-07-03 16:45:20 +0530

    changed socket connection url

--- client/src/context/SocketContext.js
+++ client/src/context/SocketContext.js
@@ -4,7 +4,7 @@ import { createContext, useEffect, useRef, useState } from "react";
 
 const SocketContext = createContext();
 
-const socket = io("http://localhost:8000");
+const socket = io("hhttps://youtubeclone-server-ozrg.onrender.com");
 
 const ContextProvider = ({ children }) => {
   const [stream, setStream] = useState(null);
